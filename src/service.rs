54afef3638b392795b479782fa05c565c412dc08
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-10-15 09:52:30 +0100

    Got MQTT working

--- src/service.rs
+++ src/service.rs
@@ -1,7 +1,6 @@
 use mosquitto_rs::*;
 use tokio::sync::mpsc::{self, Receiver, Sender};
 use tokio::sync::mpsc::{error::TryRecvError, error::TrySendError};
-use base64::{Engine as _, engine::general_purpose};
 use baby_emulator::core::{MEMORY_WORDS, BabyModel};
 use std::time::SystemTime;
 use tokio::task::JoinHandle;
@@ -40,13 +39,13 @@ async fn service_routine(mqtt: MqttConfig, rec: Receiver<Request>) {
 async fn run_model(mqtt: MqttConfig, model: BabyModel, rec: &mut Receiver<Request>) -> bool {
     let mut start = SystemTime::now();
     let mut model = if let Ok(v) = model.execute() { v } else { return true };
-    display_model(&mqtt, &model);
+    display_model(&mqtt, &model).await;
     loop {
         let time = if let Ok(v) = SystemTime::now().duration_since(start) { v.as_secs() } else { return true };
         if time >= 1 {
             start = SystemTime::now();
             model = if let Ok(v) = model.execute() { v } else { return true };
-            display_model(&mqtt, &model);
+            display_model(&mqtt, &model).await;
         }
         match rec.try_recv() {
             Err(TryRecvError::Empty) => continue,
@@ -59,7 +58,7 @@ async fn run_model(mqtt: MqttConfig, model: BabyModel, rec: &mut Receiver<Reques
     }
 }
 
-fn display_model(mqtt: &MqttConfig, model: &BabyModel) {
+async fn display_model(mqtt: &MqttConfig, model: &BabyModel) {
     let mut result: Vec<u8> = vec![0; 84];
     for i in 0..MEMORY_WORDS {
         result[i] = model.main_store[i] as u8;
@@ -67,16 +66,14 @@ fn display_model(mqtt: &MqttConfig, model: &BabyModel) {
     result[MEMORY_WORDS] = model.accumulator as u8;
     result[MEMORY_WORDS + 1] = (model.instruction_address & 0xFF) as u8;
     result[MEMORY_WORDS + 2] = ((model.instruction >> 8) + (model.instruction & 0x1F)) as u8;
-    let b64 = general_purpose::STANDARD.encode(result);
-    let _ = publish_image(b64, mqtt);
-    println!("{:?}", model)
+    result.reverse();
+    let _ = publish_image(result, mqtt).await;
 }
 
-async fn publish_image(value: String, mqtt: &MqttConfig) -> Result<(), mosquitto_rs::Error> {
+async fn publish_image(value: Vec<u8>, mqtt: &MqttConfig) -> Result<(), mosquitto_rs::Error> {
     let client = Client::with_auto_id()?;
     let _rc = client.connect(&mqtt.address, 1883, std::time::Duration::from_secs(5), None).await?;
     client.publish(&mqtt.topic, value, QoS::AtMostOnce, false)
         .await?;
     Ok(())
 }
-
